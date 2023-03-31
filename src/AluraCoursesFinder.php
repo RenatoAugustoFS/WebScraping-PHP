768b03150b5365def5632980cc1e6e63dc5cee3a
Renato Augusto <renatoaugusto.ads@gmail.com>
2023-03-31 07:46:10 -0300

    adicionando erro proposital (GITHUB ACTIONS)

--- src/AluraCoursesFinder.php
+++ src/AluraCoursesFinder.php
@@ -21,7 +21,8 @@ class AluraCoursesFinder
     {
         $response = $this->client->request('GET', $url);
         $html = $response->getBody();
-        $this->crawler->addHtmlContent($html);
+        $this->crawler->addHtmlContent(
+            $html);
         $courses = $this->crawler->filter('p.formacao-passo-nome');
         $coursesList = [];
         foreach ($courses as $course) {
